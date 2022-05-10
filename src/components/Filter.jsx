9a7208a20156814de7097ec613ce0c2f7993fca8
Aleksandr Shatskikh <work@errand.ru>
2022-05-10 11:32:22 +0300

    fix: build

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
 import { filterAction } from "../redux/tasksSlice";
 
 export default function Filter() {
